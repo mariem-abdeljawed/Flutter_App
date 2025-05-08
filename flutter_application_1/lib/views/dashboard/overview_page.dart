705433cb3e775bcb9a3c9dd7f6bd7c15bde0f45a
Mariem Abdeljawed <miryamabdeljawed@gmail.com>
2025-05-08 15:02:52 +0100

    overview page

--- flutter_application_1/lib/views/dashboard/overview_page.dart
+++ flutter_application_1/lib/views/dashboard/overview_page.dart
@@ -4,6 +4,7 @@ class OverviewPage extends StatelessWidget {
   final VoidCallback onNavigateToTransactions;
   final VoidCallback onNavigateToAddTransaction;
   final VoidCallback onNavigateToSettings;
+  final VoidCallback onNavigateToCategories; // Ajouté
   final VoidCallback onLogout;
 
   const OverviewPage({
@@ -11,6 +12,7 @@ class OverviewPage extends StatelessWidget {
     required this.onNavigateToTransactions,
     required this.onNavigateToAddTransaction,
     required this.onNavigateToSettings,
+    required this.onNavigateToCategories, // Ajouté
     required this.onLogout,
   });
 
@@ -18,7 +20,7 @@ class OverviewPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Vue d\'ensemble'),
+        title: const Text('Overview'),
         actions: [
           IconButton(
             icon: const Icon(Icons.logout),
@@ -32,16 +34,22 @@ class OverviewPage extends StatelessWidget {
           children: [
             ElevatedButton(
               onPressed: onNavigateToTransactions,
-              child: const Text('Voir les Transactions'),
+              child: const Text('View Transactions'),
             ),
             ElevatedButton(
               onPressed: onNavigateToAddTransaction,
-              child: const Text('Ajouter une Transaction'),
+              child: const Text('Add Transaction'),
             ),
             ElevatedButton(
               onPressed: onNavigateToSettings,
-              child: const Text('Paramètres'),
+              child: const Text('Settings'),
             ),
+            ElevatedButton(
+              onPressed: onNavigateToCategories, // Ajouté
+              child: const Text('Manage Categories'),
+            ),
+            
+
           ],
         ),
       ),
